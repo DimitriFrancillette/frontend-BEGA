1de12be467b0364aea368011679d453c001dadc2
Latifa <razaaly.latifa@gmail.com>
2023-05-25 21:11:04 +0200

    CSS friends screen

--- components/askFriendMessage.js
+++ components/askFriendMessage.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-const askFriendMessage = ({ message, acceptFriend, refuseFriend, userId }) => {
+const AskFriendMessage = ({ message, acceptFriend, refuseFriend, userId }) => {
 
   const friendAccepted = () => {
     acceptFriend(userId);
@@ -15,45 +16,33 @@ const askFriendMessage = ({ message, acceptFriend, refuseFriend, userId }) => {
     <View style={styles.container}>
       <Text>{message}</Text>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={friendAccepted}>
-          <Text style={styles.textButton}>Accepter</Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity onPress={friendAccepted}>
+        <FontAwesome
+          name="check-circle"
+          size={38}
+          color="green"
+        />
+      </TouchableOpacity>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={friendrefused}>
-          <Text style={styles.textButton}>Refuser</Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity onPress={friendrefused}>
+      <FontAwesome
+          name="times-circle"
+          size={38}
+          color="red"
+        />
+      </TouchableOpacity>
     </View>
   );
 };
 
-export default askFriendMessage;
+export default AskFriendMessage;
+
 styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: "100%",
+    display: "flex",
     flexDirection: "row",
-    backgroundColor: "#7935b0",
     alignItems: "center",
-    justifyContent: "flex-start",
-    borderColor: "1px solid black",
-  },
-  buttonContainer: {
-    backgroundColor: "#6B21A8",
-    borderRadius: 10,
-    marginTop: 20,
-    paddingTop: 8,
-    paddingLeft: 20,
-    paddingRight: 20,
-    alignSelf: "center",
-  },
-  textButton: {
-    color: "#DDA304",
-    fontFamily: "Inter",
-    height: 30,
-    fontWeight: "600",
-    fontSize: 16,
+    justifyContent: "space-evenly",
   },
 });
